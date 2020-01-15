8c1f22a8c3f8530998d4e486b63484b24ab7cd9f
gabriel martinez <gabriel_martinez@intuit.com>
2020-01-15 14:25:18 -0600

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.prod.spotify.com/v1/',
+  API_URL: 'https://api.spotify.com/v1/',
   REDIRECT_URI: 'https://playlist-ferromex.herokuapp.com/login/'
 };
