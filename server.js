c459590d5ceb31e7ce717e4b5955aa86ea4a2696
gabriel martinez <gabriel_martinez@intuit.com>
2020-01-14 22:35:21 -0600

    login changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/dist/playlist'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, '/dist', 'index.html'))
+res.sendFile(path.join(__dirname, '/dist/playlist/index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080, function(){
